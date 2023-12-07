a6d35108c334b423ac396a64e9b647afc99b4d98
Daniel Callaghan <dan_cal89@hotmail.com>
2023-12-07 21:20:07 +0000

    refactr(nav): remove console.log

--- components/Nav/Nav.tsx
+++ components/Nav/Nav.tsx
@@ -38,6 +38,7 @@ const Nav = ({ session }: { session: Session | null }) => {
   const searchParams = useSearchParams();
 
   useEffect(() => {
+    console.log(pathname);
     _done();
   }, [pathname, searchParams]);
 
